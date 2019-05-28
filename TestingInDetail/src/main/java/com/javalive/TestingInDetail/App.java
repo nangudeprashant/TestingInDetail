456936921b165be077d7049e980e5c231ad8cf91
Administrator <Administrator@DELL>
2019-05-28 17:56:35 +0530

    Initial check in.

---
+++ TestingInDetail/src/main/java/com/javalive/TestingInDetail/App.java
@@ -0,0 +1,13 @@
+package com.javalive.TestingInDetail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
